ae2f9194e412808fb867af44736f0b7ea502a853
swetapadmaswain <swetapadma2693@gmail.com>
2026-06-26 18:39:23 +0530

    Restore all connector imports in __init__.py

--- app/connectors/__init__.py
+++ app/connectors/__init__.py
@@ -1,5 +1,13 @@
+from .app_store import AppStoreConnector
 from .play_store import PlayStoreConnector
+from .reddit import RedditConnector
+from .forum import ForumConnector
+from .social_media import SocialMediaConnector
 
 __all__ = [
-    'PlayStoreConnector'
+    'AppStoreConnector',
+    'PlayStoreConnector',
+    'RedditConnector',
+    'ForumConnector',
+    'SocialMediaConnector'
 ]
